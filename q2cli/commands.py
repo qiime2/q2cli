53793d119df95ec014f897f7ce6392c25c1b81cd
Jorden <jakereps@users.noreply.github.com>
2016-12-15 16:23:45 -0700

    Address comments

--- q2cli/commands.py
+++ q2cli/commands.py
@@ -215,8 +215,8 @@ class ActionCommand(click.Command):
             results = action(**arguments)
 
         for result, output in zip(results, outputs):
-            click.echo('Saved to: %s' % result.save(output))
-
+            click.secho('Saved %s to: %s' % (result.type,
+                                             result.save(output)), fg='green')
 
     def handle_in_params(self, kwargs):
         import itertools
