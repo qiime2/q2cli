0f742c9207e4d9c18456bc7eba24f3d52f7f60e7
Anthony <oddant1@hotmail.com>
2019-08-30 09:19:15 -0700

    SQUASH: Addressing @ebolyen's review

--- q2cli/builtin/tools.py
+++ q2cli/builtin/tools.py
@@ -64,11 +64,11 @@ def export_data(input_path, output_path, output_format):
             if os.path.isfile(str(source)):
                 if os.path.isfile(output_path):
                     os.remove(output_path)
-                else:
+                elif os.path.dirname(output_path) == '':
                     # This allows the user to pass a filename as a path if they
                     # want their output in the current working directory
-                    if os.path.dirname(output_path) == '':
-                        output_path = os.path.join('.', output_path)
+                    output_path = os.path.join('.', output_path)
+                if os.path.dirname(output_path) != '':
                     # create directory (recursively) if it doesn't exist yet
                     os.makedirs(os.path.dirname(output_path), exist_ok=True)
                 qiime2.util.duplicate(str(source), output_path)
