6c9542207895b968ce36150ee1196b3c4066ad1e
Oddant1 <oddant1@hotmail.com>
2023-10-09 16:55:59 -0700

    bit what if. . .

--- q2cli/core/cache.py
+++ q2cli/core/cache.py
@@ -161,11 +161,10 @@ class DeploymentCache:
         for entry_point in pkg_resources.iter_entry_points(
                 group='qiime2.plugins'):
             if 'QIIMETEST' in os.environ:
-                if entry_point.name == 'dummy-plugin':
+                if entry_point.name in ('dummy-plugin', 'other-plugin'):
                     reqs.add(entry_point.dist.as_requirement())
             else:
-                if entry_point.name != 'dummy-plugin':
-                    reqs.add(entry_point.dist.as_requirement())
+                if entry_point.name not in ('dummy-plugin', 'other-plugin'):
 
         return reqs
 
