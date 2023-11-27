0afc18fd67bbe0eab593a4a8bcc0b3e0f014b56d
Johannah Hagen <hagenjohannah@gmail.com>
2023-11-27 16:40:44 -0700

    MAINT: removes outdated commands from import tools

--- q2cli/builtin/tools.py
+++ q2cli/builtin/tools.py
@@ -207,26 +207,6 @@ def show_formats(queries, importable, exportable, strict, tsv):
     _print_descriptions(descriptions, tsv)
 
 
-def show_importable_types(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    click.secho('This functionality has been moved to the list-types command.',
-                fg='red', bold=True)
-    click.secho('Run `qiime tools list-types --help` for more information.',
-                fg='red', bold=True)
-    ctx.exit()
-
-
-def show_importable_formats(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    click.secho('This functionality has been moved to the list-formats '
-                'command.', fg='red', bold=True)
-    click.secho('Run `qiime tools list-formats --help` for more information.',
-                fg='red', bold=True)
-    ctx.exit()
-
-
 @tools.command(name='import',
                short_help='Import data into a new QIIME 2 Artifact.',
                help="Import data to create a new QIIME 2 Artifact. See "
@@ -236,7 +216,7 @@ def show_importable_formats(ctx, param, value):
                     cls=ToolCommand)
 @click.option('--type', required=True,
               help='The semantic type of the artifact that will be created '
-                   'upon importing. Use --show-importable-types to see what '
+                   'upon importing. Use --list-types to see what '
                    'importable semantic types are available in the current '
                    'deployment.')
 @click.option('--input-path', required=True,
@@ -250,15 +230,8 @@ def show_importable_formats(ctx, param, value):
 @click.option('--input-format', required=False,
               help='The format of the data to be imported. If not provided, '
                    'data must be in the format expected by the semantic type '
-                   'provided via --type.')
-@click.option('--show-importable-types', is_flag=True, is_eager=True,
-              callback=show_importable_types, expose_value=False,
-              help='Show the semantic types that can be supplied to --type '
-                   'to import data into an artifact.')
-@click.option('--show-importable-formats', is_flag=True, is_eager=True,
-              callback=show_importable_formats, expose_value=False,
-              help='Show formats that can be supplied to --input-format to '
-                   'import data into an artifact.')
+                   'provided via --type. Use list-formats --importable to see '
+                   'which formats of input data are importable.')
 def import_data(type, input_path, output_path, input_format):
     import qiime2.sdk
     import qiime2.plugin
